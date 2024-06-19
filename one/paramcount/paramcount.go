b146e471c3195beda5e3a5bfe3bacb1b70c54987
Stella-Achar-Oiro <stella.achar@azubiafrica.org>
2024-06-19 15:50:16 +0300

    update paramcount

--- one/paramcount/paramcount.go
+++ one/paramcount/paramcount.go
@@ -8,6 +8,31 @@ import (
 
 func main() {
 	count := len(os.Args[1:])
-	z01.PrintRune(rune(count + '0'))
+	Paramcount := Itoa(count)
+	PrintStr(Paramcount)
+}
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
 	z01.PrintRune('\n')
+}
+
+func Itoa(n int) string {
+	if n == 0 {
+		return "0"
+	}
+	sign := ""
+	if n < 0 {
+		n = -n
+		sign = "-"
+	}
+	q := ""
+	for n > 0 {
+		digits := n % 10
+		q = string(rune('0'+digits)) + q
+		n /= 10
+	}
+	return sign + q
 }
